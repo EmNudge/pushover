8d8ad51fafad27ae668077ac4532b67fb4dfd8e4
emnudge <emnudge@gmail.com>
2020-01-26 00:33:43 -0500

    added type prototype tests

--- src/utils/combinators/prototype.test.ts
+++ src/utils/combinators/prototype.test.ts
@@ -1,104 +1,135 @@
 import { expect } from 'chai';
 import { prototypeParser, param } from './prototype';
-import { Type } from '../index'
+import { Type } from '../index';
 
 const checkCombinator = ({ res, shouldFail = false, expectedRes = res.result }) => {
-  // should fail or not
-  expect(res.isError).to.be[String(shouldFail)];
+	// should fail or not
+	expect(res.isError).to.be[String(shouldFail)];
 
-  // deep or regular equality if result is an object
-  const equality = typeof res.result === 'object' ? 'eql' : 'equal';
-  expect(res.result).to[equality](expectedRes);
+	// deep or regular equality if result is an object
+	const equality = typeof res.result === 'object' ? 'eql' : 'equal';
+	expect(res.result).to[equality](expectedRes);
 
-  // if doesn't fail, but it's supposed to (or vice versa), log it
-  if (res.isError !== shouldFail) console.log(res);
+	// if doesn't fail, but it's supposed to (or vice versa), log it
+	if (res.isError !== shouldFail) console.log(res);
 };
 
 describe('Prototype Parsing', () => {
-  it('should parse a prototype parameter', () => {
-    const res1 = param.run('myName: number')
-    const res2 = param.run('myName: number | string')
-    const res3 = param.run('myName:number|string')
-    const res4 = param.run('myName  :  number  |   string')
-    
-    checkCombinator({ res: res1, expectedRes: { name: 'myName', types: [Type.Number]} });
-    checkCombinator({ res: res2, expectedRes: { name: 'myName', types: [Type.Number, Type.String]} });
-    checkCombinator({ res: res3, expectedRes: { name: 'myName', types: [Type.Number, Type.String]} });
-    checkCombinator({ res: res4, expectedRes: { name: 'myName', types: [Type.Number, Type.String]} });
-  })
-  
-  it('should parse a list of params', () => {
-    const res1 = prototypeParser.run('name: string, age: number | string')
+	it('should parse a prototype parameter', () => {
+		const res1 = param.run('myName: number');
+		const res2 = param.run('myName: number | string');
+		const res3 = param.run('myName:number|string');
+		const res4 = param.run('myName  :  number  |   string');
 
-    checkCombinator({ 
-      res: res1, 
-      expectedRes: { 
-        args: [
-          { name: 'name', types: [Type.String] },
-          { name: 'age', types: [Type.Number, Type.String] },
-        ], 
-        optional: null
-      }
-    });
-  })
-  
-  it('should parse optional params', () => {
-    const res1 = prototypeParser.run('name: string[, age: number | string]')
+		checkCombinator({ res: res1, expectedRes: { name: 'myName', types: [Type.Number] } });
+		checkCombinator({ res: res2, expectedRes: { name: 'myName', types: [Type.Number, Type.String] } });
+		checkCombinator({ res: res3, expectedRes: { name: 'myName', types: [Type.Number, Type.String] } });
+		checkCombinator({ res: res4, expectedRes: { name: 'myName', types: [Type.Number, Type.String] } });
+	});
 
-    checkCombinator({ 
-      res: res1, 
-      expectedRes: { 
-        args: [
-          { name: 'name', types: [Type.String] },
-        ], 
-        optional: {
-          args: [
-            { name: 'age', types: [Type.Number, Type.String] },
-          ],
-          optional: null,
-        }
-      }
-    });
-  })
+	it('should parse a list of params', () => {
+		const res1 = prototypeParser.run('name: string, age: number | string');
 
-  it('should parse nested optional params', () => {
-    const res1 = prototypeParser.run('name: string[, age: number | string[, channel: channel]]')
-    const res2 = prototypeParser.run('name: string[, age: number | string, channel: channel]')
+		checkCombinator({
+			res: res1,
+			expectedRes: {
+				args: [{ name: 'name', types: [Type.String] }, { name: 'age', types: [Type.Number, Type.String] }],
+				optional: null
+			}
+		});
+	});
 
-    checkCombinator({ 
-      res: res1, 
-      expectedRes: { 
-        args: [
-          { name: 'name', types: [Type.String] },
-        ], 
-        optional: {
-          args: [
-            { name: 'age', types: [Type.Number, Type.String] },
-          ],
-          optional: {
-            args: [
-              { name: 'channel', types: [Type.Channel] },
-            ],
-            optional: null,
-          },
-        }
-      }
-    });
+	it('can parse all types', () => {
+    const res1 = prototypeParser.run('a: number | string | boolean | function | channel | link | user | role');
+		const res2 = prototypeParser.run(
+			'a: number, b: string, c: boolean, d: function, e: channel, f: link, g: user, h: role'
+		);
 
-    checkCombinator({ 
-      res: res2, 
-      expectedRes: { 
-        args: [
-          { name: 'name', types: [Type.String] },
-        ], 
-        optional: {
-          args: [
-            { name: 'age', types: [Type.Number, Type.String] },
-            { name: 'channel', types: [Type.Channel] },
-          ],
-          optional: null,
-        }
-      }
-    });
-  })
+		checkCombinator({
+			res: res1,
+			expectedRes: {
+				args: [
+					{
+						name: 'a',
+						types: [
+							Type.Number,
+							Type.String,
+							Type.Boolean,
+							Type.Function,
+							Type.Channel,
+							Type.Link,
+							Type.User,
+							Type.Role
+						]
+					}
+				],
+				optional: null
+			}
+		});
+
+		checkCombinator({
+			res: res2,
+			expectedRes: {
+				args: [
+					{ name: 'a', types: [Type.Number] },
+					{ name: 'b', types: [Type.String] },
+					{ name: 'c', types: [Type.Boolean] },
+					{ name: 'd', types: [Type.Function] },
+					{ name: 'e', types: [Type.Channel] },
+					{ name: 'f', types: [Type.Link] },
+					{ name: 'g', types: [Type.User] },
+					{ name: 'h', types: [Type.Role] }
+				],
+				optional: null
+			}
+		});
+	});
+
+	it('should parse optional params', () => {
+		const res1 = prototypeParser.run('name: string[, age: number | string]');
+
+		checkCombinator({
+			res: res1,
+			expectedRes: {
+				args: [{ name: 'name', types: [Type.String] }],
+				optional: {
+					args: [{ name: 'age', types: [Type.Number, Type.String] }],
+					optional: null
+				}
+			}
+		});
+	});
+
+	it('should parse nested optional params', () => {
+		const res1 = prototypeParser.run('name: string[, age: number | string[, channel: channel]]');
+		const res2 = prototypeParser.run('name: string[, age: number | string, channel: channel]');
+
+		checkCombinator({
+			res: res1,
+			expectedRes: {
+				args: [{ name: 'name', types: [Type.String] }],
+				optional: {
+					args: [{ name: 'age', types: [Type.Number, Type.String] }],
+					optional: {
+						args: [{ name: 'channel', types: [Type.Channel] }],
+						optional: null
+					}
+				}
+			}
+		});
+
+		checkCombinator({
+			res: res2,
+			expectedRes: {
+				args: [{ name: 'name', types: [Type.String] }],
+				optional: {
+					args: [
+						{ name: 'age', types: [Type.Number, Type.String] },
+						{ name: 'channel', types: [Type.Channel] }
+					],
+					optional: null
+				}
+			}
+		});
+	});
 });
